de5acb94755964a2c4d92cf1b6343d7ee4282edf
Niyazi Özdinç ÇELİKEL <niyazi.celikel@ulakhaberlesme.com.tr>
2021-09-16 11:44:08 +0300

    add some functions
    Signed-off-by: Niyazi Özdinç ÇELİKEL <niyazi.celikel@ulakhaberlesme.com.tr>

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# RVALUE REFERENCE
++ To move the object that the `lvalue reference` points to, one can move the object directly 
+    - std::move is used to `convert to rvalue reference`, it is exactly equivalent to a *static_cast to an rvalue reference*.
++ A named rvalue is an lvalue as all Named objects are lvalues
++ Rvalue references _can only bind_ to Rvalues
+
+# LVALUE REFERENCE
++ A reference that `binds to an lvalue`, lvalue reference is marked with a single ampersand (*&*).
+
 # Compile & Run
 ```bash
 g++ references.cpp -o references ; ./$_
--- references.cpp
+++ references.cpp
@@ -12,7 +12,7 @@ struct Payload {
 } Str;
 
 void bar(Payload p) {
-  std::cout << "Got \"Payload\" by value\n";
+  std::cout << "bar()::Got \"Payload\" by value\n";
   std::cout << p.c << "\n";
   std::cout << p.i << "\n";
   std::cout << p.r << "\n";
@@ -21,9 +21,38 @@ void bar(Payload p) {
   std::cout << p.s << "\n";
 }
 
+void baz(Payload &p) {
+  std::cout << "baz()::Got \"Payload\" by reference\n";
+  std::cout << p.c << "\n";
+  std::cout << p.i << "\n";
+  std::cout << p.r << "\n";
+  std::cout << p.y << "\n";
+  std::cout << p.b << "\n";
+  std::cout << p.s << "\n";
+}
+
+void foo(Payload &&p) { // rvalue arguments will select this function
+  std::cout << "Accepts rvalue reference as an argument.\n";
+}
+
 int main() {
   Payload payloadd{'o', 17, 'c', 91, true, "OzdincCelikel"};
   Payload &&refref = std::move(payloadd);
   bar(refref);
+  baz(refref);
+  // foo(refref);
+  // an rvalue reference cannot be bound to an lvalue
+  std::cout << "================================================\n";
+  bar(Payload{'s', 41, 'a', 93, true, "SimgeAlkin"});
+  // baz(Payload{'s', 41, 'a', 93, true, "SimgeAlkin"});
+  // initial value of reference to non-const must be an lvalue
+  // foo(refref); //an rvalue reference cannot be bound to an lvalue
+  foo(Payload{'s', 41, 'a', 93, true, "SimgeAlkin"});
+  std::cout << "================================================\n";
+  bar(std::move(payloadd));
+  // baz(std::move(payloadd));
+  // initial value of reference to non-const must be an lvalue
+  foo(std::move(payloadd));
+  std::cout << "================================================\n";
   return 0;
 }
